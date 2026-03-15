c4ecaf8e3b4b5ea150d40e289e161743dd73ce9c
Vladyslav Korenevskyi <25588519+tmbkoren@users.noreply.github.com>
2026-03-15 15:32:19 -0700

    basic i18n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { useTranslations } from 'next-intl';
+import styles from './page.module.css';
 
 export default function Home() {
-
-  const arr = [
-    {"name" : "tarkov",
-      "value" : 300,
-      "id" : 1
-    },
-    {"name" : "bebra",
-      "value" : 200,
-      "id" : 2
-    },
-    {"name" : "amogus",
-      "value" : 100,
-      "id" : 3
-    },
-  ]
-
-  return (
-    <div className={styles.page}>
-      {arr.map(item => {
-        <Row name={item.name} value={item.value} onDelete={() => {}}/>
-      })}
-      
-    </div>
-  );
-}
-
-function Row({name, value, onDelete}) {
-  return <div>
-    <p>{name}</p>
-    <p>{value}</p>
-    <button onClick={() => {onDelete}}/>
-  </div>
+  const t = useTranslations('HomePage');
+  return <h1 className={styles.title}>{t('title')}</h1>;
 }
